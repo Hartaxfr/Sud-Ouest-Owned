a81a156ae57ecd14ccad53552062d73235226a00
U-AIBTP\averry <averry@dev-02.aibtp.fr>
2016-03-31 16:46:19 +0200

    clean code and add info end of article

--- popup.js
+++ popup.js
@@ -3,15 +3,22 @@ document.addEventListener('DOMContentLoaded', function() {
   checkPageButton.addEventListener('click', function() {
     chrome.tabs.getSelected(null, function(tab) {
       if ( tab.url.indexOf("sudouest.fr") > -1){
-        var scriptSO = "document.querySelector('div.entry-content div.short').style.display = 'none';document.querySelector('div.entry-content div.long').style.display = 'block';var articlePastille = document.querySelector('h2.entry-summary span.pastille-qualif.premium');articlePastille.style.backgroundColor  = 'red';articlePastille.innerHTML = 'OWNED';";
+
+        var scriptSO = "";
+        scriptSO += "document.querySelector('div.entry-content div.short').style.display = 'none';";
+        scriptSO += "document.querySelector('div.entry-content div.long').style.display = 'block';";
+        scriptSO += "document.querySelector('h2.entry-summary span.pastille-qualif.premium').style.backgroundColor  = 'red';";
+        scriptSO += "document.querySelector('h2.entry-summary span.pastille-qualif.premium').innerHTML = 'OWNED';";
+        scriptSO += "document.querySelector('section.article-premium-locked div.premium-bottom').style.display = 'none';";
+        scriptSO += "document.querySelector('section.article-premium-locked div.info-text').style.backgroundColor = 'red';";
+        scriptSO += "document.querySelector('section.article-premium-locked div.info-text').style.padding = '10px';";
+        scriptSO += "document.querySelector('section.article-premium-locked div.info-text').style.color = 'white';";
+        scriptSO += "document.querySelector('section.article-premium-locked div.info-text').innerHTML = 'Cet article a été débloqué par <a style=\"color:white;text-decoration: underline;\"href=\"http://alexandreverry.fr\">alexandreverry.fr</a>';";
 
         chrome.tabs.executeScript(tab.id, {code: scriptSO}, function(response)
         {
-    
+          console.log("Sud-ouest owned.");
         });
-
-
-        
       }
     });
   }, false);
